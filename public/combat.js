bd79d03ddd28e4ac447b53259d7d0f5565363ea4
entity-3034228 <entity.3034228@gmail.com>
2026-02-25 17:41:16 +0000

    Enhance combat log messages for clarity

--- public/combat.js
+++ public/combat.js
@@ -7,6 +7,7 @@ function startCombat(type) {
         enemy = { name: "Goblin Guard", health: 20, attack: 6, defense: 2, gold: 12 };
     }
 
+    clearLog();
     log("A " + enemy.name + " appears!");
     drawEnemy();
     combatMenu();
@@ -23,10 +24,10 @@ function combatMenu() {
 function playerAttack() {
     let damage = Math.max(1, player.attack - enemy.defense);
     enemy.health -= damage;
-    log("You hit for " + damage + " damage!");
+    log("You hit the " + enemy.name + " for " + damage + " damage!");
 
-    if (enemy.health <= 0) {
-        log("You defeated the " + enemy.name);
+    if(enemy.health <= 0){
+        log("You defeated the " + enemy.name + "!");
         player.gold += enemy.gold;
         inCombat = false;
         villageMenu();
@@ -39,18 +40,19 @@ function playerAttack() {
 function enemyAttack() {
     let damage = Math.max(1, enemy.attack - player.defense);
     player.health -= damage;
-    log("Enemy hits for " + damage);
+    log("The " + enemy.name + " hits you for " + damage + " damage!");
 
-    if (player.health <= 0) {
+    if(player.health <= 0){
         alert("GAME OVER");
         location.reload();
+        return;
     }
 
     combatMenu();
 }
 
 function attemptFlee() {
-    if (Math.random() < 0.5) {
+    if(Math.random() < 0.5){
         log("You escaped!");
         inCombat = false;
         villageMenu();
@@ -58,4 +60,13 @@ function attemptFlee() {
         log("Failed to flee!");
         enemyAttack();
     }
+}
+
+// Simple enemy drawing
+function drawEnemy() {
+    ctx.clearRect(250, 50, 300, 200);
+    ctx.fillStyle = "darkred";
+    ctx.fillRect(300, 100, 100, 80); // enemy body
+    ctx.fillStyle = "black";
+    ctx.fillText(enemy.name, 320, 90);
 }
